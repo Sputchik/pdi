fba4ec8d04b93300bdbffde881ffa0fc45714063
Sputchik <sputchik@gmail.com>
2025-09-18 17:50:30 +0200

    ffmpeg

--- pdi.bat
+++ pdi.bat
@@ -470,6 +470,13 @@ if exist "FFmpeg.zip" (
 	robocopy /NJH /NJS /NFL /NDL /NP /NS /NC /COPYALL /E /MOVE "FFmpeg" "%PF%\FFmpeg"
 	call :SetPath "%PF%\FFmpeg\bin\"
 )
+if exist "FFmpeg_libsvt-av1-psy.zip" (
+	echo Installing FFmpeg...
+	tar -xf "FFmpeg_libsvt-av1-psy.zip"
+	move "ffmpeg-*" "FFmpeg"
+	robocopy /NJH /NJS /NFL /NDL /NP /NS /NC /COPYALL /E /MOVE "FFmpeg" "%PF%\FFmpeg"
+	call :SetPath "%PF%\FFmpeg\bin\"
+)
 if exist "LibAvif.zip" (
 	echo Installing LibAvif...
 	call :Extract "LibAvif"
